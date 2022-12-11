9d19140771ce2e81e84aa5b63a47fb708089c765
sydbalcom <sbalcom3@gatech.edu>
2022-12-11 11:46:02 -0500

    add stacked bar chart without labels or filtering

--- final-project/test/main.js
+++ final-project/test/main.js
@@ -1,8 +1,28 @@
-var svg = d3.select('svg');
+var mapSVG = d3.select("#map");
+var chartSVG = d3.select("#chart");
 
 // width and height of map
 let w = 800;
 let h = 800;
+
+// width and height of chart svg
+var chartSVGwidth = 550;
+var chartSVGheight = 450;
+
+// width and height of chart
+var chartWidth = 350;
+var chartHeight = 300;
+
+var barHeight = 50;
+
+// array that stores positions of each bar (necessary to make stacked bars)
+var barPositions = []
+
+var chartPadding = 75;
+
+// axes of chart
+var xAxis, yAxis;
+var xScale, yScale;
 
 const csv = []; // array that holds relevant questions from all countries and all identities
 const countryScores = new Map(); // key: countryname, value: current (?) score
@@ -54,7 +74,7 @@ d3.csv('LGBT_Survey_DailyLife.csv').then(function(dataset, json) {
     d3.json('europeMed.json').then(function(json) {
         initializeData(dataset, json)
         // binding data and creating one path per GeoJSON feature
-        var enter = svg.selectAll('g')
+        var enter = mapSVG.selectAll('g')
             .data(json.features)
             .enter()
             .append('g');
@@ -81,7 +101,7 @@ d3.csv('LGBT_Survey_DailyLife.csv').then(function(dataset, json) {
                 }
             })
             .on('mouseover', function(d, i) {
-                svg.append('text').text(d.properties.NAME)
+                mapSVG.append('text').text(d.properties.NAME)
                     .attr('class', 'hovertext')
                     .attr('id', 'hoveringtext')
                     .attr('x', function(data, i) {
@@ -93,8 +113,29 @@ d3.csv('LGBT_Survey_DailyLife.csv').then(function(dataset, json) {
                     .attr('pointer-events', 'none');
             })
             .on('mouseout', function(d, i) {
-                console.log(d3.select('#hoveringtext'));
+                // console.log(d3.select('#hoveringtext'));
                 d3.select('#hoveringtext').remove();
+            })
+            .on('click', function(d, i) {
+                // initially hide bar chart
+                hideBarChart();
+
+                // array that holds relevant question for selected country and all identities
+                var countryCSV = [];
+
+                // only include info for relevant country
+                // testing with first question initially - should change later
+                for (let i = 0; i < dataset.length; ++i) {
+                    if ((dataset[i].CountryCode == d.properties.NAME) && 
+                        (dataset[i].question_code == 'b1_a')) {
+                        countryCSV.push(dataset[i]);
+                    }
+                }
+
+                // only update chart if the country has data
+                if (countryCSV.length > 0) {
+                    updateBarChart(countryCSV);
+                }
             });
     });
 });
@@ -124,4 +165,87 @@ function calculateScore(countryName) {
     }
     countryScores.set(countryName, score);
     return {score: score, found: found};
+}
+
+function hideBarChart() {
+    // could change this to span to include everything?
+    chartSVG.selectAll('g').remove();
+}
+
+function updateBarChart(countryCSV) {
+
+    // initialize bar chart
+
+    //x axis
+    xScale = d3.scaleLinear()
+        .domain([0, 100])
+        .range([0, chartWidth]);
+
+    xAxis = d3.axisBottom(xScale);
+
+    chartSVG.append('g')
+        .attr("transform", function() {
+            return "translate(" + (chartSVGwidth - chartWidth) / 2
+            + "," + (chartSVGheight - chartPadding) + ")"
+        })
+        .call(xAxis);
+
+    // y axis
+    yScale = d3.scaleOrdinal()
+        .domain(["Lesbian", "Gay", "Bisexual women", "Bisexual men", "Transgender", ""])
+        .range([0, chartHeight * 1/5, chartHeight * 2/5, chartHeight * 3/5, chartHeight * 4/5, chartHeight]);
+
+    yAxis = d3.axisLeft(yScale);
+
+    chartSVG.append('g')
+        .attr("transform", function() {
+            return "translate(" + (chartSVGwidth - chartWidth) / 2
+            + "," + (chartPadding) + ")"
+        })
+        .call(yAxis);
+
+    // add bars
+    var barElements = chartSVG.selectAll('.bar')
+        .data(countryCSV, function(d) {
+            return d.subset;
+        })
+
+    //create bars and append to bar elements
+    var barEnter = barElements.enter().append('g').attr("class", "bar");
+
+    barEnter
+        .append('rect')
+        .attr("width", function(d, i) {
+            return xScale(d.percentage);
+        })
+        .attr("height", barHeight)
+        .attr("transform", function(d, i) {
+            
+            // the position of each bar is dependent on the width of the bars before it
+            // if the bar is the first of the subset (i % 5 == 0), then it should
+            // start at the y axis
+            var barPosition = (i % 5 == 0 ? 0 : xScale(countryCSV[i - 1].percentage));
+            if (i % 5 != 0) {
+                barPositions[i] = barPositions[i - 1] + barPosition;
+            } else {
+                barPositions[i] = (chartSVGwidth - chartWidth) / 2;
+            }
+
+            return "translate(" + barPositions[i]
+            + "," + (yScale(d.subset) - (barHeight / 2) + chartPadding) + ")"
+        })
+        .attr("fill", function(d, i) {
+            if (d.answer == "Very widespread") {
+                return '#ff5035'
+            } else if (d.answer == "Fairly widespread") {
+                return '#ff7c68'
+            } else if (d.answer == "Fairly rare") {
+                return '#ffa89b'
+            } else if (d.answer == "Very rare") {
+                return '#ffc3ba'
+            } else {
+                return '#e9e9e9'
+            }
+        });
+
 }
