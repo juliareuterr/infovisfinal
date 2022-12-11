b7ff93ec8744a31be2361ee4d616b59b9b0243c2
juliareuterr <48806848+juliareuterr@users.noreply.github.com>
2022-12-11 16:25:57 -0500

    buttons!!!

--- final-project/test/main.js
+++ final-project/test/main.js
@@ -1,5 +1,10 @@
+var topSVG = d3.select('#top');
 var mapSVG = d3.select("#map");
 var chartSVG = d3.select("#chart");
+
+// width and height of top svg
+const topSVGwidth = 1350;
+const topSVGheight = 200;
 
 // width and height of map
 let w = 800;
@@ -20,6 +25,9 @@ var barPositions = []
 
 var chartPadding = 75;
 
+const titlePadding = 50;
+const buttonBorder = 20;
+
 // axes of chart
 var xAxis, yAxis;
 var xScale, yScale;
@@ -28,35 +36,80 @@ const csv = []; // array that holds relevant questions from all countries and al
 const countryScores = new Map(); // key: countryname, value: current (?) score
 const countryPercentages = new Map(); // key: countryname, value: array of percentages to each answer, [0] is very widespread, [3] is very rare
 
-function initializeData(dataset, json) {
-    for (let i = 0; i < dataset.length; ++i) {
-        if (dataset[i].question_code == 'b1_a' || dataset[i].question_code == 'b1_b' 
-        || dataset[i].question_code == 'b1_c' || dataset[i].question_code == 'b1_d') {
-        csv.push(dataset[i]);
-        }
-        // b1_a is offensive language by politicians, b1_b is casual jokes
-        // b1_c is expressions of hatred and aversion, b1_d is assaults and harrassment
-    }
-    // loop through added ones, check country
-    for (let i = 0; i < csv.length; i++) {
-        let name = csv[i].CountryCode;
-        countryScores.set(name, 0);
-        let temp = countryPercentages.get(name);
-        if (temp != null && temp.length > 0) {
-            if (csv[i].answer == 'Very widespread') {
-                temp[0] += csv[i].percentage;
-            } else if (csv[i].answer == 'Fairly widespread') {
-                temp[1] += csv[i].percentage;
-            } else if (csv[i].answer == 'Fairly rare') {
-                temp[2] += csv[i].percentage;
-            } else if (csv[i].answer == 'Very rare') {
-                temp[3] += csv[i].percentage;
-            }
-        } else {
-            countryPercentages.set(name, [0,0,0,0]);
-        }
-    }
+topSVG.append('text')
+    .text('Hostility Towards LGBT People in the EU')
+    .attr('class', 'title')
+    .attr('x', titlePadding)
+    .attr('y', titlePadding);
+
+topSVG.append('text')
+    .text('In your opinion, how widespread is...')
+    .attr('class', 'qheader')
+    .attr('x', titlePadding)
+    .attr('y', titlePadding*2);
+
+topSVG.append('text')
+    .text('... about lesbian, gay, bisexual, and/or transgender people by politicians in the country where you live?')
+    .attr('class', 'qheader')
+    .attr('x', topSVGwidth - titlePadding)
+    .attr('text-anchor', 'end')
+    .attr('y', titlePadding*2+80);
+
+const buttonsText = ['Offensive Language', 'Casual Jokes', 'Expressions of Hatred and Aversion', 'Assaults and Harrassment'];
+const buttonsLength = [];
+const rectX = [];
+
+const selectedButtons = new Map();
+for (let i = 0; i < 4; i++) {
+    selectedButtons.set(i, false);
 }
+
+var questionButtons = topSVG.selectAll('rect')
+    .data(buttonsText)
+    .enter();
+
+questionButtons.append('rect')
+    .attr('id', function(d, i) {return 'rect' + i;})
+    .attr('class', 'qbutton')
+    .attr('y', titlePadding*7/3)
+    .attr('rx', 20)
+    .attr('ry', 20)
+
+questionButtons.append('text')
+    .text(function(d, i) {return buttonsText[i];})
+    .attr('class', 'qtext')
+    .attr('y', titlePadding*7/3 + 25)
+    .each(function(d, i) { buttonsLength[i] = this.getComputedTextLength()});
+
+questionButtons.selectAll('rect')
+    .attr('width', function(d, i) { 
+        return buttonsLength[i] + buttonBorder; 
+    })
+    .attr('x', function(d, i) {
+        if (i == 0) {
+            rectX[i] = titlePadding;
+        }
+        else {
+            rectX[i] = (buttonBorder*2 + Number(d3.select(this.previousSibling).attr('x')) + Number(buttonsLength[i-1]))
+        }
+        return rectX[i];
+    })
+    .on('click', function(d, i) {
+        if (selectedButtons.get(i) == true) {
+            selectedButtons.set(i, false);
+            topSVG.select('#rect' + i).attr('class', 'qbutton');
+        } else {
+            selectedButtons.set(i, true);
+            topSVG.select('#rect' + i).attr('class', 'qbuttonpressed');
+        }
+        updateCountries();
+    });
+questionButtons.selectAll('.qtext')
+    .attr('x', function(d, i) { 
+        if (i==0) return titlePadding + buttonsLength[i]/2 + buttonBorder/2;
+        else return (rectX[i] + buttonsLength[i]/2 + buttonBorder/2);//(30 + Number(d3.select(this.previousSibling).attr('x')) + Number(buttonsLength[i])/2);
+    });
+
 
 d3.csv('LGBT_Survey_DailyLife.csv').then(function(dataset, json) {
     // defining map projection
@@ -82,24 +135,7 @@ d3.csv('LGBT_Survey_DailyLife.csv').then(function(dataset, json) {
         enter.append('path')
             .attr('d', path)
             .attr('class', 'normalpath')
-            .attr('fill', function(d, i) {
-                // conditional fill here https://www.color-hex.com/color-palette/1020309
-                let obj = calculateScore(d.properties.NAME);
-                if (obj.found) {
-                    var s = obj.score;
-                    if (s > 1000) { // best
-                        return '#ffc3ba';
-                    } else if (s > 900) {
-                        return '#ffa89b';
-                    } else if (s > -100) {
-                        return '#ff7c68'
-                    } else { // worst
-                        return '#ff5035';
-                    }
-                } else {
-                    return '#e9e9e9'
-                }
-            })
+            .attr('fill', '#e9e9e9')
             .on('mouseover', function(d, i) {
                 mapSVG.append('text').text(d.properties.NAME)
                     .attr('class', 'hovertext')
@@ -140,7 +176,70 @@ d3.csv('LGBT_Survey_DailyLife.csv').then(function(dataset, json) {
     });
 });
 
-function calculateScore(countryName) {
+function initializeData(dataset, json) {
+    for (let i = 0; i < dataset.length; ++i) {
+        if (dataset[i].question_code == 'b1_a' || dataset[i].question_code == 'b1_b' 
+        || dataset[i].question_code == 'b1_c' || dataset[i].question_code == 'b1_d') {
+        csv.push(dataset[i]);
+        }
+        // b1_a is offensive language by politicians, b1_b is casual jokes
+        // b1_c is expressions of hatred and aversion, b1_d is assaults and harrassment
+    }
+    // loop through added ones, check country
+    initializeScores();
+}
+
+function initializeScores() {
+    for (let i = 0; i < csv.length; i++) {
+        let name = csv[i].CountryCode;
+        countryScores.set(name, 0);
+        let temp = countryPercentages.get(name);
+        if (temp != null && temp.length > 0) {
+            if (csv[i].answer == 'Very widespread') {
+                temp[0] += csv[i].percentage;
+            } else if (csv[i].answer == 'Fairly widespread') {
+                temp[1] += csv[i].percentage;
+            } else if (csv[i].answer == 'Fairly rare') {
+                temp[2] += csv[i].percentage;
+            } else if (csv[i].answer == 'Very rare') {
+                temp[3] += csv[i].percentage;
+            }
+        } else {
+            countryPercentages.set(name, [0,0,0,0]);
+        }
+    }
+}
+
+function updateCountries() {
+    initializeScores();
+    var numofbuttons = 0;
+    for (let i = 0; i < 4; i++) {
+        if (selectedButtons.get(i) == true) {
+            numofbuttons += 1;
+        }
+    }
+    mapSVG.selectAll('g')
+        .selectAll('path')
+            .attr('fill', function(d, i) {
+                let obj = updateScore(d.properties.NAME);
+                if (obj.found) {
+                    var s = obj.score;
+                    if (s > (250 * numofbuttons)) { // best
+                        return '#ffc3ba';
+                    } else if (s > (225 * numofbuttons)) {
+                        return '#ffa89b';
+                    } else if (s > (-25 * numofbuttons)) {
+                        return '#ff7c68'
+                    } else { // worst
+                        return '#ff5035';
+                    }
+                } else {
+                    return '#e9e9e9'
+                }
+            });
+}
+
+function updateScore(countryName) {
     var score;
     if (countryScores.has(countryName)) {
         score = Number(countryScores.get(countryName));
@@ -148,18 +247,56 @@ function calculateScore(countryName) {
         score = 0;
     }
     var found = false;
-
+    // check which questions are selected
     for (let i = 0; i < csv.length; i++) {
         if (csv[i].CountryCode == countryName) {
-            found = true;
-            if (csv[i].answer == 'Very widespread') { // very widespread hate = lower score
-                score -= Number(csv[i].percentage);
-            } else if (csv[i].answer == 'Fairly widespread') {
-                score -= 0;
-            } else if (csv[i].answer == 'Fairly rare') {
-                score += Number(csv[i].percentage);
-            } else if (csv[i].answer == 'Very rare') {
-                score += Number((2 * csv[i].percentage));
+            if (selectedButtons.get(0) && csv[i].question_code == "b1_a") {
+                var found = true;
+                if (csv[i].answer == 'Very widespread') { // very widespread hate = lower score
+                    score -= Number(csv[i].percentage);
+                } else if (csv[i].answer == 'Fairly widespread') {
+                    score -= 0;
+                } else if (csv[i].answer == 'Fairly rare') {
+                    score += Number(csv[i].percentage);
+                } else if (csv[i].answer == 'Very rare') {
+                    score += Number((2 * csv[i].percentage));
+                }
+            }
+            if (selectedButtons.get(1) && csv[i].question_code == "b1_b") {
+                var found = true;
+                if (csv[i].answer == 'Very widespread') { // very widespread hate = lower score
+                    score -= Number(csv[i].percentage);
+                } else if (csv[i].answer == 'Fairly widespread') {
+                    score -= 0;
+                } else if (csv[i].answer == 'Fairly rare') {
+                    score += Number(csv[i].percentage);
+                } else if (csv[i].answer == 'Very rare') {
+                    score += Number((2 * csv[i].percentage));
+                }
+            }
+            if (selectedButtons.get(2) && csv[i].question_code == "b1_c") {
+                var found = true;
+                if (csv[i].answer == 'Very widespread') { // very widespread hate = lower score
+                    score -= Number(csv[i].percentage);
+                } else if (csv[i].answer == 'Fairly widespread') {
+                    score -= 0;
+                } else if (csv[i].answer == 'Fairly rare') {
+                    score += Number(csv[i].percentage);
+                } else if (csv[i].answer == 'Very rare') {
+                    score += Number((2 * csv[i].percentage));
+                }
+            }
+            if (selectedButtons.get(3) && csv[i].question_code == "b1_d") {
+                var found = true;
+                if (csv[i].answer == 'Very widespread') { // very widespread hate = lower score
+                    score -= Number(csv[i].percentage);
+                } else if (csv[i].answer == 'Fairly widespread') {
+                    score -= 0;
+                } else if (csv[i].answer == 'Fairly rare') {
+                    score += Number(csv[i].percentage);
+                } else if (csv[i].answer == 'Very rare') {
+                    score += Number((2 * csv[i].percentage));
+                }
             }
         }
     }
@@ -170,6 +307,10 @@ function calculateScore(countryName) {
 function hideBarChart() {
     // could change this to span to include everything?
     chartSVG.selectAll('g').remove();
+}
+
+function updateScoreBars(countryName, identity) {
+    
 }
 
 function updateBarChart(countryCSV) {
@@ -249,3 +390,4 @@ function updateBarChart(countryCSV) {
         });
 
 }
+
